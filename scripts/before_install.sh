9d92c9a09132fa904584a41b38eb017b812976b1
lehoanglong95 <lehoanglong95@gmail.com>
2024-02-01 22:12:34 +1100

    change before install file

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-if [ "$(git diff master..HEAD --name-only | grep requirements.txt)" ]; then
-  echo "Requirements.txt has changed. Building Docker image and starting services..."
-  cd /home/ubuntu/my_custom_airflow
-  docker build . --tag my_custom_airflow_with_requirements
-  docker-compose up
-else
-  echo "No changes in requirements.txt. Skipping Docker build and service restart."
-fi
+cd /home/ec2-user/job_crawler
+docker build . --tag my_custom_airflow_with_requirements
+docker-compose up

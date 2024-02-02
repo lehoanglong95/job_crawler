4c38edfca73a985e298bf1cc9ee644cb475d8123
lehoanglong95 <lehoanglong95@gmail.com>
2024-02-02 16:57:53 +1100

    before install

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-set -e
+#set -e
 
-cd /home/ec2-user/job_crawler
+#cd /home/ec2-user/job_crawler
+files=$(ls)
+
+# Echo the captured output
+echo "List of files in the current directory:"
+echo "$files"
 docker build . --tag my_custom_airflow_with_requirements
 docker-compose up

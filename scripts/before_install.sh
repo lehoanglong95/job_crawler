3d306a4e9f975327baa07d67108a02a4e40b0fe1
lehoanglong95 <lehoanglong95@gmail.com>
2024-02-02 17:01:00 +1100

    change appspec

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,11 +2,7 @@
 
 #set -e
 
-#cd /home/ec2-user/job_crawler
-files=$(ls)
+cd /home/ec2-user/job_crawler_v1
 
-# Echo the captured output
-echo "List of files in the current directory:"
-echo "$files"
 docker build . --tag my_custom_airflow_with_requirements
 docker-compose up

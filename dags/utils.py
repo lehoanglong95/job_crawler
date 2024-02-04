9530b407161a87ee85d29265cfd92709320f3089
lehoanglong95 <lehoanglong95@gmail.com>
2024-02-04 17:39:19 +1100

    remove try catch in upload to s3

--- dags/utils.py
+++ dags/utils.py
@@ -33,28 +33,28 @@ def chunk(input: List[Any], number_of_chunks=50):
 def save_to_s3(list_data: List[dict]):
     for data in list_data:
         if not data["job_info"] and not data["job_description"]:
-            continue
-        try:
-            crawled_url_hash = hash_string(data["crawled_url"])
-            file_name = f"{crawled_url_hash}.txt"
-            file_path = os.path.join(data["crawled_website"], file_name)
+            print("DO NOT PROCESS")
+        # try:
+        crawled_url_hash = hash_string(data["crawled_url"])
+        file_name = f"{crawled_url_hash}.txt"
+        file_path = os.path.join(data["crawled_website"], file_name)
 
-            # Check if the output folder exists; if not, create it
-            if not os.path.exists(data["crawled_website"]):
-                os.makedirs(data["crawled_website"])
+        # Check if the output folder exists; if not, create it
+        if not os.path.exists(data["crawled_website"]):
+            os.makedirs(data["crawled_website"])
 
-            combination_text = f"url: {data['crawled_url']}\n\n{data['job_info']}\n\n{data['job_description']}"
+        combination_text = f"url: {data['crawled_url']}\n\n{data['job_info']}\n\n{data['job_description']}"
 
-            # Write content to the file
-            with open(file_name, 'w', encoding='utf-8') as file:
-                file.write(combination_text)
+        # Write content to the file
+        with open(file_name, 'w', encoding='utf-8') as file:
+            file.write(combination_text)
 
-            print(f"file exist: {file_name in os.listdir()}")
-            s3 = boto3.client('s3')
-            s3.upload_file(file_name, "lhl-job-descriptions", file_path)
-            print("do not remove file after upload")
+        print(f"file exist: {file_name in os.listdir()}")
+        s3 = boto3.client('s3')
+        s3.upload_file(file_name, "lhl-job-descriptions", file_path)
+        print("do not remove file after upload")
             # time.sleep(10)
             # os.remove(file_name)
-        except Exception as e:
-            print(f"create file fail with error: {e}")
+        # except Exception as e:
+        #     print(f"create file fail with error: {e}")
 
